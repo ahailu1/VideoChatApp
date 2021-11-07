eb6454aeb3b8525bac3251c870d92c5ed9660156
ahailu1 <alex.hailu@hotmail.com>
2021-11-07 18:12:39 -0500

    updated

--- VideoApi/config/express.js
+++ VideoApi/config/express.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const cors = require("cors");
 
 const router = express.Router();
-const server = require("http").createServer(app);
+
 const { port } = require("./config.js");
+
+const certificatePath = path.resolve(path.join(__dirname, "../certificates"));
+const options = {
+  key: fs.readFileSync(`${certificatePath}/key.pem`),
+  cert: fs.readFileSync(`${certificatePath}/cert.pem`),
+};
+const server = require("http").createServer(app);
 
 const initServer = () => {
   server.listen(port, (err) => {
+    console.log(err);
     console.log(`server listening on port${port}`);
   });
 };

6ad5dc13dbfdbb99a30d2765819808d62fd2091d
root <root@DESKTOP-77B3VJT.localdomain>
2022-02-24 11:19:29 -0500

    updated

--- videoUI/src/components/videochatpage/utilities/modal.js
+++ videoUI/src/components/videochatpage/utilities/modal.js
@@ -67,7 +67,7 @@ const VideoModal = ({setActiveKey,socket,userdata,setModal,onHide,setFriendInfo,
 <Row className = {styles.container__row}>
 
     <Col className = {`d-flex flex-column align-items-center ${styles.container__image}`} lg = {2} xl = {6}>
-    <Image className = {styles.modal__image} src = {`${process.env.React_APP_SITE_IMAGE_URL}`} thumbnail rounded />
+    <Image className = {styles.modal__image} src = {`${process.env.React_APP_SITE_IMAGE_URL}--${modalInfo.sender_id}--profilepicture.jpg`} thumbnail rounded />
     
     <div className = {styles.container__username}>
     <span className = {styles.modal__username}>{modalInfo.username} </span>
